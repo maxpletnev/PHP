b77ab17e3bcf6ce8df8811259e683a4669bc87db
Max <pletnevm2008@rambler.ru>
2020-01-20 15:50:43 +0300

    Задание по 4 уроку.

---
+++ Lesson_4/exercise-1/config/main.php
@@ -0,0 +1,9 @@
+<?php
+define("PUBLIC_DIR", "./");
+define("ENGINE_DIR", PUBLIC_DIR . "engine/");
+define("IMAGES_DIR", PUBLIC_DIR . "img/");
+define("ROOT_DIR", PUBLIC_DIR . "../");
+define("CONFIG_DIR", ROOT_DIR . "config/");
+define("UPLOADS_DIR", ROOT_DIR . "uploads/");
+© 2020 GitHub, Inc.
+?>
